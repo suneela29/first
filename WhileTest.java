8a172fdb4337744ea8d5cfdc08022b09765448c7
suneela29 <48209177+suneela29@users.noreply.github.com>
2020-08-09 18:54:34 +0530

    while test added

---
+++ WhileTest.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class WhileTest{
+    public static void main(String[] args)
+    {
+        int n=7;
+        int i=0;
+        while(i<n)
+        {
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
